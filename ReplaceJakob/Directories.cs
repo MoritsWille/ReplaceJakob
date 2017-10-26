afaa348865bd70721390fb88b9c44eca13e7a7a7
MoritsWille <moritsbw@gmail.com>
2017-10-26 07:41:38 +0200

    Added pdf to image system

---
+++ ReplaceJakob/Directories.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ReplaceJakob
+{
+    class Directories
+    {
+        public static List<string> ListPdfs(string directory)
+        {
+            List<string> names = new List<string>();
+            
+            foreach (string file in Directory.GetFiles(directory))
+            {
+                if (file.Contains(".pdf"))
+                {
+                    names.Add(file);
+                }
+            }
+            return names;
+        }
+
+        public static List<string> ListImgs (string directory)
+        {
+            List<string> names = new List<string>();
+            foreach (string pdf in ListPdfs(directory))
+            {
+                names.Add(Path.GetFileNameWithoutExtension(pdf) + ".png");
+            }
+
+            return names;
+        }
+
+        public static string Images = Environment.CurrentDirectory + @"\temps\";
+        public static string NoPrints = Environment.CurrentDirectory + @"\Dont Print\";
+        public static string BWPrints = Environment.CurrentDirectory + @"\Print without color\";
+        public static string ColorPrints = Environment.CurrentDirectory + @"\Print with color\";
+
+    }
+}
--- ReplaceJakob/Program.cs
+++ ReplaceJakob/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ImageMagick;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,34 @@ namespace ReplaceJakob
     {
         static void Main(string[] args)
         {
+            string read = Console.ReadLine();
+
+            Directory.CreateDirectory(Directories.Images);
+            Directory.CreateDirectory(Directories.BWPrints);
+            Directory.CreateDirectory(Directories.ColorPrints);
+            Directory.CreateDirectory(Directories.NoPrints);
+
+
+            Directory.SetCurrentDirectory(read);
+            MagickReadSettings settings = new MagickReadSettings();
+            // Settings the density to 300 dpi will create an image with a better quality
+            settings.Density = new Density(300, 300);
+
+            foreach (string fileName in Directories.ListPdfs(read))
+            {
+                using (MagickImageCollection images = new MagickImageCollection())
+                {
+                    // Add all the pages of the pdf file to the collection
+                    images.Read(fileName, settings);
+                    IMagickImage image = images[0];
+
+                        // Writing to a specific format works the same as for a single image
+                        image.Format = MagickFormat.Png;
+                        image.Write(Directories.Images + Path.GetFileNameWithoutExtension(Path.GetFileName(fileName)) + ".png");
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
